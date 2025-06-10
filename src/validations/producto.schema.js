18d2f39bd974bd7c32f4922ea0483a24ef5e9251
GomezFabricio <fabricio.gomez4371@gmail.com>
2025-06-10 12:11:46 -0300

    Corregí la validación de categoria_id para que sea un número positivo en lugar de una cadena no vacía.

--- src/validations/producto.schema.js
+++ src/validations/producto.schema.js
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const productoSchema = z.object({
   producto_nombre: z.string().min(2, { message: 'El nombre del producto es obligatorio y debe tener al menos 2 caracteres.' }),
-  categoria_id: z.string().nonempty({ message: 'La categoría es obligatoria.' }),
+  categoria_id: z.number().int().positive({ message: 'La categoría es obligatoria y debe ser un número positivo.' }),
   producto_descripcion: z.string().nullable(), // Permitir valores null
   producto_precio_venta: z.number().positive({ message: 'El precio de venta debe ser un número positivo.' }).nullable(),
   producto_precio_costo: z.number().positive({ message: 'El precio de costo debe ser un número positivo.' }).nullable(),
